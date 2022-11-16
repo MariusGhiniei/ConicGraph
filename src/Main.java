17b5bc86c1f748dd96971d8599d5487f4d49cfd2
MariusGhiniei <marius_iulian_23@yahoo.com>
2022-11-16 02:11:38 +0200

    Info Panel - UI

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,20 @@ class Conic extends Frame{
 
         inPanel = new InputPanel(this);
         add(inPanel);
-        inPanel.setBounds(0,0,width/2,height/2);
+        inPanel.setBounds(0,0,width/2,height/2+20);
 
         outPanel = new OutputPanel(this);
         add(outPanel);
-        outPanel.setBounds(0,height/2+1,width/2,height/2);
+        outPanel.setBounds(0,height/2+20,width/2,height/2-20);
 
         drawPanel = new DrawPanel(this);
         add(drawPanel);
         drawPanel.setBounds(width/2+1,0,width/2,height);
 
-        resize(width,height);
+        resize(width-10,height);
         move(0,0);
         setVisible(true);
+
 
     }
     public static void main(String args[]){
@@ -47,18 +48,43 @@ class Conic extends Frame{
     class InputPanel extends Panel{
         public int a11,a12,a22,a13,a23,a33;
         Font f = new Font("TimesRoman",1,14);
+        Font g = new Font("TimesRoman",2,10);
+
+        Font h = new Font("TimesRoman",1,16);
+
 
         public InputPanel(Conic conic){
-            setBackground(Color.RED);
-            Label info = new Label("rezolvarea ecuatiei: ..");
-            add(info);
-            info.setBounds(30,30,width/6,height/6);
-            TextField tf1 = new TextField("a11: ");
-            TextField tf2 = new TextField("a12: ");
-            TextField tf3 = new TextField("a22: ");
-            TextField tf4 = new TextField("a13: ");
-            TextField tf5 = new TextField("a23: ");
-            TextField tf6 = new TextField("a33: ");
+
+            setBackground(Color.DARK_GRAY);
+
+            setLayout(new GridLayout(8,2));
+
+            Label resolve = new Label("     Rezolvarea ecuatiei: ",Label.CENTER);
+            Label equation = new Label("a11 x^2 + 2*a12 xy + a22 y^2 + 2*a13 x + 2*a23 y + a33 = 0",
+                    Label.CENTER);
+            resolve.setFont(f);
+            equation.setFont(g);
+
+            TextField tf1 = new TextField();
+            TextField tf2 = new TextField();
+            TextField tf3 = new TextField();
+            TextField tf4 = new TextField();
+            TextField tf5 = new TextField();
+            TextField tf6 = new TextField();
+
+            Label l1 = new Label("a11: ",Label.CENTER);
+            Label l2 = new Label("a12: ",Label.CENTER);
+            Label l3 = new Label("a22: ",Label.CENTER);
+            Label l4 = new Label("a13: ",Label.CENTER);
+            Label l5 = new Label("a23: ",Label.CENTER);
+            Label l6 = new Label("a33: ",Label.CENTER);
+
+            l1.setFont(h);
+            l2.setFont(h);
+            l3.setFont(h);
+            l4.setFont(h);
+            l5.setFont(h);
+            l6.setFont(h);
 
             tf1.setForeground(Color.cyan);
             tf2.setForeground(Color.cyan);
@@ -67,29 +93,33 @@ class Conic extends Frame{
             tf5.setForeground(Color.cyan);
             tf6.setForeground(Color.cyan);
 
-            tf1.setFont(f);
-            tf2.setFont(f);
-            tf3.setFont(f);
-            tf4.setFont(f);
-            tf5.setFont(f);
-            tf6.setFont(f);
+            tf1.setFont(g);
+            tf2.setFont(g);
+            tf3.setFont(g);
+            tf4.setFont(g);
+            tf5.setFont(g);
+            tf6.setFont(g);
 
-            add(tf1);
-            add(tf2);
-            add(tf3);
-            add(tf4);
-            add(tf5);
-            add(tf6);
-            Button btn1 = new Button("idk");
-            add(btn1);
-            btn1.setLocation(100,100);
-            tf1.setBounds(100,15,100,25);
+            add(resolve); add(equation);
 
-            tf2.setBounds(120,35,100,25);
-            tf3.setBounds(120,60,100,25);
-            tf4.setBounds(120,85,100,25);
-            tf5.setBounds(120,110,100,25);
-            tf6.setBounds(120,135,100,25);
+            add(l1); add(tf1);
+
+            add(l2); add(tf2);
+
+            add(l3); add(tf3);
+
+            add(l4); add(tf4);
+
+            add(l5); add(tf5);
+
+            add(l6); add(tf6);
+
+            Button draw = new Button("Deseneaza");
+            draw.setFont(g);
+            Label drawNull = new Label();
+
+            add(draw); add(drawNull);
+
         }
 
     }
